1c2da2ce99833ffceb356ddb684ac02d7f04ba13
HollywoodOKC <T_Lane11@yahoo.com>
2024-01-13 21:01:15 -0600

    chore: Added navbar, fixed GitHub icon.

---
+++ src/components/Header.tsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+    return (
+        <>
+            <nav className="navbar navbar-inverse">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                        <a className="navbar-brand" href="#">AwesomePM</a>
+                    </div>
+                    <div className="collapse navbar-collapse" id="myNavbar">
+                        <ul className="nav navbar-nav">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Page 1</a></li>
+                            <li><a href="#">Page 2</a></li>
+                            <li><a href="#">Page 3</a></li>
+                        </ul>
+                        <ul className="nav navbar-nav navbar-right">
+                            <li><a href="#"><span className="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                            <li><a href="#"><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+
+    )
+}
+
+export default Header;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,8 @@ import { Link } from "react-router-dom";
 
 function Home() {
     return (
-        <><h1 className="text-3xl font-bold underline">Empty landing page</h1>
+        <>
+            <h1 className="text-3xl font-bold underline">Empty landing page</h1>
             <Link to="/login">Log in here</Link>
         </>
     );
